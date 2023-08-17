05047845a485d422c07512f1c8a440876e8b9b84
Rubidium7 <kyllikki.fillman@gmail.com>
2023-08-17 11:43:15 +0300

    started up structuring the code

---
+++ includes/irc.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   irc.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlonka <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 11:37:48 by nlonka            #+#    #+#             */
+/*   Updated: 2023/08/17 11:41:38 by nlonka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IRC_HPP
+# define IRC_HPP
+
+# include <iostream>
+# include <string>
+# include "defines.hpp"
+
+//errors.c
+int	print_error(t_error_code type);
+
+#endif
