07c129936343d76b95935710b44f6a039f9c6d43
Rubidium7 <kyllikki.fillman@gmail.com>
2023-10-26 18:23:42 +0300

    added checking for weird characters in user

--- includes/User.hpp
+++ includes/User.hpp
@@ -12,6 +12,8 @@ class User
 		static void userCommand(int socket, Client &client, std::vector<std::string> args, bool debug);
 
 	private:
+		static bool			_weirdCharsInRealName(int socket, std::string nick, bool debug, std::vector<std::string> args);
+		static bool			_weirdChars(std::string &name);
 		static std::string	_parseRealName(std::vector<std::string> args);
 
 		User();
